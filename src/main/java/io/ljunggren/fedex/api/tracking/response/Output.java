6b66370129f606839acffdd464087787ae42c4d8
aljunggren <aljunggren@allegient.com>
2022-01-22 10:17:02 -0500

    Package name update

---
+++ src/main/java/io/ljunggren/fedex/api/tracking/response/Output.java
@@ -0,0 +1,25 @@
+package io.ljunggren.fedex.api.tracking.response;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(Include.NON_NULL)
+public class Output {
+
+    private List<CompleteTrackResult> completeTrackResults;
+    @JsonProperty("alerts")
+    private String alert;
+    
+}
---
+++ src/test/java/io/ljunggren/fedex/api/tracking/response/TrackingResponseTest.java
@@ -0,0 +1,34 @@
+package io.ljunggren.fedex.api.tracking.response;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import io.ljunggren.jsonUtils.JsonUtils;
+
+public class TrackingResponseTest {
+    
+    private String readFromResources(String file) throws IOException {
+        return IOUtils.toString(this.getClass().getResourceAsStream(file), "UTF-8");
+    }
+
+    @Test
+    public void deserializeTest() throws IOException {
+        String json = readFromResources("/trackingResponse.json");
+        TrackingResponse response = JsonUtils.jsonToObject(json, TrackingResponse.class);
+        assertNotNull(response);
+    }
+    
+    @Test
+    public void serializeTest() throws IOException {
+        String json = readFromResources("/trackingResponse.json");
+        TrackingResponse response = JsonUtils.jsonToObject(json, TrackingResponse.class);
+        String serializedResponse = JsonUtils.objectToJson(response);
+        assertTrue(JsonUtils.areEqual(json, serializedResponse));
+    }
+
+}
