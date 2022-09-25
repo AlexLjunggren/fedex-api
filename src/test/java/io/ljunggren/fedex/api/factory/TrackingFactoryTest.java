e7f5cfd5b7d8b151e4daf6a970a9434dc0142031
Alex Ljunggren <aljunggren@dminc.com>
2022-09-25 16:13:14 -0400

    Added test

--- src/test/java/io/ljunggren/fedex/api/factory/TrackingFactoryTest.java
+++ src/test/java/io/ljunggren/fedex/api/factory/TrackingFactoryTest.java
@@ -1,6 +1,7 @@
 package io.ljunggren.fedex.api.factory;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -11,6 +12,11 @@ import io.ljunggren.fedex.api.tracking.request.TrackingRequest;
 
 public class TrackingFactoryTest {
 
+    @Test
+    public void instantiationTest() {
+        assertNotNull(new TrackingFactory());
+    }
+    
     @Test
     public void basicRequestTest() throws JsonProcessingException {
         String[] tackingNumbers = new String[] {"449044304137821", "149331877648230", "020207021381215"};
